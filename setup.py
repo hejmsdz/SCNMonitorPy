0217a83e2249243ad13dab303d21d92e3a211b3c
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2016-10-15 21:54:01 +0200

    Create a base package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SCNMonitor',
+      version='0.1',
+      description='Data usage monitor for Poznan University of Technology dorms',
+      author='Mikołaj Rozwadowski',
+      author_email='mikolaj@mrozwadowski.com',
+      url='https://mrozwadowski.com/projects/scnmonitor/',
+      packages=['scnmonitor'],
+     )
